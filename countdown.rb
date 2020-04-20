14f98e877b98bfeb8635b271cf2e527dcc486a5c
Joey <joey.alzamora@gmail.com>
2020-04-20 19:12:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,13 +5,22 @@ def countdown(int)
    puts "#{int} SECOND(S)!" 
   int -= 1
  end
+ 
  "HAPPY NEW YEAR!"
+ 
 end
 
+
 def countdown_with_sleep(int)
-  int == sleep 60
-  
+  while int > 0 
+   puts "#{int} SECOND(S)!" 
+  int -= 1
+ end
+ 
+ "HAPPY NEW YEAR!"
+
 end
+  
 
 countdown(10)
 
